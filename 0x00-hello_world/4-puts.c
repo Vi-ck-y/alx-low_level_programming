b761bf9bee7c31ac366a1bb65f67173a023f341a
Victoria <victoriaomeireoluchi@gmail.com>
2023-03-16 19:23:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+/**
+*main - main entry point
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
